c71869a2dc10482518deb2980f6444e7b406d7d6
Daniel Acuña <suchious@gmail.com>
2016-08-22 12:15:01 -0300

    Clean db on startup

--- src/server/index.js
+++ src/server/index.js
@@ -6,8 +6,10 @@ import routes from './routes';
 import stocks from './lib/stock-service';
 import _ from 'lodash';
 import redis from './lib/redis';
+import { pub } from './lib/redis';
 
 redis.init(process.env.REDIS_URL);
+pub.flushdb()
 
 stocks.start()
 app.register(routes);
