82cf30d78d1a0a35cf85a72f57297914145fcccb
Daniel Acuña <suchious@gmail.com>
2016-08-22 13:08:20 -0300

    Notify nginx and listen at required port

--- src/server/app.js
+++ src/server/app.js
@@ -4,13 +4,15 @@ import Hapi from 'hapi';
 import inert from 'inert';
 import hoek from 'hoek';
 import dotenv from 'dotenv';
+import touch from 'touch';
 
 // Load .env, if any.
 dotenv.config({ silent: true });
 
 // Initialize the server
 const server = new Hapi.Server();
-server.connection({ port: process.env.PORT || 5000 });
+const port = process.env.NODE_ENV == 'production' ? '/tmp/nginx.socket' : process.env.PORT || 5000
+server.connection({ port: port });
 
 // Register plugins
 server.register(inert);
@@ -21,5 +23,8 @@ server.start( (err) => {
 	if(err) throw err;
 	console.log(`Server running at ${server.info.uri}`);
 });
+
+if(process.env.NODE_ENV == 'production')
+	touch.sync('/tmp/app-initialized')
 
 export default server;
